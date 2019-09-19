9322f5a4a05ee3db7f3917138733d44cd9182b7b
yuanliang.chen <yuanliang.chen@uaes.com>
2019-09-19 23:20:28 +0800

    a

---
+++ 15.IO/io-test/src/main/java/com/fang2chen/java/base/study/io/test/FileTest.java
@@ -0,0 +1,24 @@
+package com.fang2chen.java.base.study.io.test;
+
+import java.io.File;
+
+/**
+ * <p>
+ *     File类使用说明
+ * </p>
+ *
+ * @author yuanliang.chen
+ * @date 2019/9/19 22:15
+ */
+public class FileTest {
+
+    public static void main(String[] args) {
+
+        System.out.println(File.pathSeparator);
+        System.out.println(File.pathSeparatorChar);
+        System.out.println(File.separator);
+        System.out.println(File.separatorChar);
+
+    }
+
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-"# java-base-learn" 
+# java base learn
+
+大部分都是《java疯狂讲义中的内容》
